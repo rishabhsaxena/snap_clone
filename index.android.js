4e2cfc20a6884b2a8e8c209b1bfaf474560b5947
Rishabh Saxena <rishabh.robben@gmail.com>
2016-05-20 21:03:14 +0530

    init

---
+++ index.android.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import { AppRegistry } from 'react-native';
+
+import AppContainer from './js/AppContainer';
+
+AppRegistry.registerComponent('SnapClone', () => AppContainer);
---
+++ js/AppContainer.js
@@ -0,0 +1,18 @@
+import React,{ Component } from 'react'
+
+import {	
+	View,
+	Text,
+} from 'react-native';
+
+export default class AppContainer extends Component {
+
+	render() {
+		return (
+			<View>
+				<Text>container</Text>
+			</View>
+		);
+	}
+}
+
